b06d034fdf2f894e594b422dabf728106eb4e1ea
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2021-06-23 00:10:59 -0300

    feat: include navbar component in index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,50 +1,17 @@
 import Head from 'next/head'
 
+import { Navbar } from '../components/Navbar'
+
 export default function Home() {
   return (
-    <div className="container">
+    <>
       <Head>
-        <title>E-commerce</title>
+        <title>Geral.com</title>
       </Head>
 
       <main>
-        <h1>E-commerce</h1>
+        <Navbar />
       </main>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
+    </>
   )
 }
