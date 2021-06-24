9836d9441df9b4ea11656da12856114bcb342a10
João Paulo <jpaulope@indracompany.com>
2021-06-24 11:09:39 -0300

    style: cart

--- src/pages/carrinho/index.tsx
+++ src/pages/carrinho/index.tsx
@@ -3,6 +3,7 @@ import { useContext } from 'react';
 import { CartContext } from '../../contexts/CartContext';
 import { Navbar } from '../../components/Navbar'
 import { ProductType } from '../../types/products';
+import Image from 'next/image'
 
 import styles from './styles.module.css';
 
@@ -22,7 +23,45 @@ export default function Carrinho() {
       </header>
 
       <main className="container py-5">
-      <h1 className={styles.mediumTitle}>Meu Carrinho</h1>
+        <div className="row">
+          <div className="col-lg-7 col-sm-12 mb-4">
+            <h1 className={`${styles.mediumTitle} mb-4`}>Meu Carrinho</h1>
+            <div className={`"card mr-sm-5" ${styles['my-shadow']}`}>
+              <div className="card-body border-none">
+                <div className="row">
+                  <div className="col-md-3 col-sm-12">
+                    <Image
+                      src="/images/camisa-barcelona.svg"
+                      alt="camisa do barcelona"
+                      layout="fill"
+                      objectFit="cover"
+                      quality={100}
+                    />
+                  </div>
+                  <div className="col-md-5 col-sm-12">
+                    <h5 className={`card-title ${styles["smallTitle"]}`}>Camisa Barcelona 20/21 S/Nº Torcedor Nike Masculina</h5>
+                    <div>
+                      <p className={`card-text ${styles["smallText"]} ${styles['mb-restart-3']}`}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy.</p>
+                      <p className={`${styles.smallText} ${styles['mb-restart-3']}`}><b>Tamanho:</b> P</p>
+                      <p className={`${styles.smallText} ${styles['mb-restart-3']}`}><b>Cor:</b>: Azul</p>
+                    </div>
+                  </div>
+                  <div className="col-md-4 col-sm-12">
+                    <img
+                      src="/icons/garbage.svg"
+                      alt="Remova item do carrinho"
+                      aria-label="Remova item do carrinho"
+                      className={styles.absoluteRigthTop}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-5 col-sm-12">
+            <h1 className={styles.mediumTitle}>Resumo da Compra</h1>
+          </div>
+        </div>
       </main>
     </>
   )
