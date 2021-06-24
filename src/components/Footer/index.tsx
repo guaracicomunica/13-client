1b1bf7096fe47d2fc1b7580797d4d595eb6f1a3d
João Paulo <jpaulope@indracompany.com>
2021-06-24 14:29:04 -0300

    style: responsive footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -6,36 +6,36 @@ export function Footer() {
             <div className="container">
                 <div className="row mt-5">
                     <div className="col-sm-12 col-md-4">
-                        <div className="d-flex align-item-center justify-content-center mb-3">
+                        <div className="d-flex align-item-center mb-3">
                             <img src="/icons/wallet.svg" className="mr-2" alt="até" />
                             <span className="align-middle">Até <b>3x vezes sem juros</b> no cartão</span>
                         </div>
                     </div>
                     <div className="col-sm-12 col-md-4">
-                        <div className="d-flex align-item-center justify-content-center mb-3">
+                        <div className="d-flex align-item-center mb-3">
                             <img src="/icons/truck.svg" className="mr-2" alt="até" />
                             <span className="align-middle">Entregamos para <b>todo o Brasil</b></span>
                         </div>
                     </div>
                     <div className="col-sm-12 col-md-4">
-                        <div className="d-flex align-item-center justify-content-center mb-3">
+                        <div className="d-flex align-item-center mb-3">
                             <img src="/icons/security.svg" className="mr-2" alt="até" />
                             <span className="align-middle">Compre com <b> segurança</b> no cartão</span>
                         </div>
                     </div>
                 </div>
                 <div className="row">
-                    <div className="col-sm-12 d-flex justify-content-center">
-                        <hr className={`${styles['background-white']}  w-75`} />
+                    <div className={`${styles['hr-container']}  col-sm-12`}>
+                        <hr className={`${styles['hr']}  w-75`} />
                     </div>
                 </div>
                 <div className="row mt-4">
-                    <div className="col-md-1 col-sm-12 d-flex mb-3">
+                    <div className="col-md-2 col-sm-12 d-flex mb-3">
                         <div>
                             <img src="/icons/logo-white.svg" alt="Geral.com" />
                         </div>
                     </div>
-                    <div className="col-md-3 col-sm-12 d-flex align-item-center justify-content-center mb-3">
+                    <div className="col-md-3 col-sm-12 d-flex align-item-center mb-3">
                         <ul className={`${styles['ul']}`}>
                             <li className={`${styles['li']} mb-1`}><b>MENU</b></li>
                             <li className={`${styles['li']} mb-1`}>
@@ -49,7 +49,7 @@ export function Footer() {
                             </li>
                         </ul>
                     </div>
-                    <div className="col-md-3 col-sm-12 d-flex align-item-center justify-content-center mb-3">
+                    <div className="col-md-3 col-sm-12 d-flex align-item-center mb-3">
                         <ul className={`${styles['ul']}`}>
                             <li className={`${styles['li']} mb-1`}><b>ATENDIMENTO</b></li>
                             <li className={`${styles['li']} mb-1`}>
@@ -63,8 +63,8 @@ export function Footer() {
                             </li>
                         </ul>
                     </div>
-                    <div className="col-md-4 col-sm-12">
-                        <div className="d-flex justify-content-center align-items-center">
+                    <div className="col-md-4 col-sm-12 pb-5">
+                        <div className="d-flex align-items-center">
                             <div className={`${styles['social-media-box']} d-flex justify-content-center align-items-center mr-3`}>
                                 <a href="#">
                                     <img src="/icons/twitter.svg" alt="twitter" />
