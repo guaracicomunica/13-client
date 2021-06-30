c5f0d45bc6155cb4dcffc3db1482a5f06c087ba6
João Paulo <jpaulope@indracompany.com>
2021-06-29 21:35:56 -0300

    style: name field

--- src/pages/cadastro/index.tsx
+++ src/pages/cadastro/index.tsx
@@ -34,12 +34,26 @@ export default function Cadastro() {
                     <div className="col-sm-12 d-flex justify-content-center">
                         <form action="javascript:void(0)" className={`${styles['w-sm-75']} ${styles['w-md-50']}`}>
                             <div className="form-group">
-                                <label htmlFor="email"  className={`${styles['label']}`}>E-mail</label>
+                                <label htmlFor="email" className={`${styles['label']}`}>E-mail</label>
                                 <input type="email" className={`${styles['bg-input']} form-control`} name="email" placeholder="usuario@email.com" />
                             </div>
-                            <div className="form-group w-50">
-                                <label htmlFor="email" className={`${styles['label']}`}>Telefone</label>
-                                <input type="email" className={`${styles['bg-input']} form-control`} name="text" placeholder="(84) 99999-9999" />
+                            <div className="form-group">
+                                <label htmlFor="name" className={`${styles['label']}`}>Nome</label>
+                                <input type="text" className={`${styles['bg-input']} form-control`} name="name" placeholder="Fulano da Silva Lima" />
+                            </div>
+                            <div className="row">
+                                <div className="col-md-6 col-sm-12">
+                                    <div className="form-group">
+                                        <label htmlFor="cpf" className={`${styles['label']}`}>CPF</label>
+                                        <input type="cpf" className={`${styles['bg-input']} form-control`} name="text" placeholder="000.000.000-00" />
+                                    </div>
+                                </div>
+                                <div className="col-md-6 col-sm-12">
+                                    <div className="form-group">
+                                        <label htmlFor="phone" className={`${styles['label']}`}>Telefone</label>
+                                        <input type="text" className={`${styles['bg-input']} form-control`} name="phone" placeholder="(84) 99999-9999" />
+                                    </div>
+                                </div>
                             </div>
                             <div className="form-group">
                                 <label htmlFor="password" className={`${styles['label']}`}>Criar sua senha</label>
