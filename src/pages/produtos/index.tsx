b386acfc064b2b43096873ca63a8d381f63ed298
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2021-08-02 16:49:59 -0300

    feat: create filter component responsive

--- src/pages/produtos/index.tsx
+++ src/pages/produtos/index.tsx
@@ -2,13 +2,13 @@ import { useState, useEffect } from 'react';
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import Link from 'next/link';
-import { Range, getTrackBackground } from 'react-range';
 import ReactPaginate from 'react-paginate';
 
 import { getAPIClient } from '../../services/apiClient';
 
 import Carousel from '../../components/Carousel';
 import { Loader } from '../../components/Loader';
+import { Filter } from '../../components/Filter';
 import { ProductCard } from '../../components/ProductCard';
 
 import styles from './styles.module.css';
@@ -31,8 +31,6 @@ type ProdutosPageProps = {
 
 export default function Produtos(props: ProdutosPageProps) {
   const api = getAPIClient();
-
-  const [initialPosition, setInitialPosition] = useState([25,200]);
 
   const [loading, setLoading] = useState(false);
 
@@ -77,6 +75,10 @@ export default function Produtos(props: ProdutosPageProps) {
     setLoading(false);
   }
 
+  function openFilter() {
+    document.querySelector('#filter')?.classList.toggle("show-filter");
+  }
+
   return (
     <>
       <Head>
@@ -110,15 +112,9 @@ export default function Produtos(props: ProdutosPageProps) {
         </section>
 
         <div className={`${styles["filter-bar"]} d-flex d-md-none w-100`}>
-          <a
-            data-toggle="collapse"
-            href="#filter"
-            role="button"
-            aria-expanded="false"
-            aria-controls="filter"
-          >
+          <button className={styles["filter-collapse"]} onClick={openFilter}>
             <img src="/icons/filter.svg" alt="Filtro" />
-          </a>
+          </button>
         </div>
         
         <section className="section align-items-center py-0">
@@ -151,190 +147,7 @@ export default function Produtos(props: ProdutosPageProps) {
         </section>
         
         <section className={`section ${styles["products-filter"]}`}>
-          <div className={`${styles.filter} d-none d-md-flex flex-column py-5 px-4`}>
-            <select name="size" id="size" className="form-control mb-3">
-              <option selected={true} unselectable="off">Tamanho</option>
-              <option value="pp">PP</option>
-              <option value="p">P</option>
-              <option value="m">M</option>
-              <option value="g">G</option>
-              <option value="gg">GG</option>
-            </select>
-
-            <select name="brand" id="brand" className="form-control mb-3">
-              <option selected={true} unselectable="off">Marca</option>
-              <option value="nike">Nike</option>
-              <option value="adidas">Adidas</option>
-            </select>
-
-            <select name="type-product" id="type-product" className="form-control mb-3">
-              <option selected={true} unselectable="off">Tipo de produto</option>
-              <option value="camisa">Camisa</option>
-            </select>
-
-            <select name="material" id="material" className="form-control mb-3">
-              <option selected={true} unselectable="off">Material</option>
-              <option value="algodao">Algodão</option>
-              <option value="poliester">Poliéster</option>
-            </select>
-
-            <select name="rating" id="rating" className="form-control mb-3">
-              <option selected={true} unselectable="off">Avaliações</option>
-              <option value="five">Cinco estrelas</option>
-              <option value="four">Quatro estrelas</option>
-              <option value="three">Três estrelas</option>
-              <option value="two">Duas estrelas</option>
-              <option value="one">Uma estrela</option>
-            </select>
-
-            <div className={`${styles["filter-checkbox"]} mb-3`}>
-              <input
-                type="checkbox"
-                name="selecao-tailandesa"
-                id="selecao-tailandesa"
-              />
-              <label htmlFor="selecao-tailandesa">Seleções tailandesas</label>
-              <div className={`${styles["icon-checkbox"]}`} />
-            </div>
-
-            <div className={`${styles["filter-checkbox"]} mb-3`}>
-              <input
-                type="checkbox"
-                name="selecao-europeia"
-                id="selecao-europeia"
-              />
-              <label htmlFor="selecao-europeia">Seleções europeias</label>
-              <div className={`${styles["icon-checkbox"]}`} />
-            </div>
-
-            <div className={`${styles["filter-checkbox"]} mb-4`}>
-              <input
-                type="checkbox"
-                name="selecao-brasileira"
-                id="selecao-brasileira"
-              />
-              <label htmlFor="selecao-brasileira">Seleções brasileiras</label>
-              <div className={`${styles["icon-checkbox"]}`} />
-            </div>
-            
-            <hr />
-
-            <h5 className="my-4">Preço</h5>
-            <div
-              style={{
-                display: 'flex',
-                flexWrap: 'wrap'
-              }}
-            >
-              <Range
-                values={initialPosition}
-                min={19.99}
-                max={599.99}
-                onChange={(initialPosition) => {
-                  setInitialPosition(initialPosition);
-                }}
-                renderTrack={({ props, children }) => (
-                  <div
-                    onMouseDown={props.onMouseDown}
-                    onTouchStart={props.onTouchStart}
-                    style={{
-                      ...props.style,
-                      height: '36px',
-                      display: 'flex',
-                      width: '100%'
-                    }}
-                  >
-                    <div
-                      ref={props.ref}
-                      style={{
-                        height: '3px',
-                        width: '100%',
-                        background: getTrackBackground({
-                          values: initialPosition,
-                          colors: ['#E5E5E5', '#2C3238', '#E5E5E5'],
-                          min: 19.99,
-                          max: 599.99
-                        })
-                      }}
-                    >
-                      {children}
-                    </div>
-                  </div>
-                )}
-                renderThumb={({ props, isDragged }) => (
-                  <div
-                    {...props}
-                    style={{
-                      ...props.style,
-                      height: '16px',
-                      width: '16px',
-                      border: '4px solid #2C3238',
-                      borderRadius: '50%',
-                      backgroundColor: '#FFF',
-                      display: 'flex',
-                      justifyContent: 'center',
-                      alignItems: 'center'
-                    }}
-                  />
-                )}
-              />
-              <output style={{ color: '#D4D4D4', marginBottom: '24px' }} id="output">
-                R$ {initialPosition[0].toFixed(2)} - R$ {initialPosition[1].toFixed(2)}
-              </output>
-            </div>
-
-            <hr />
-
-            <h5 className="my-4">Cores</h5>
-            <div className={`mb-4 ${styles.colors}`}>
-              <div className={styles.color}>
-                <input type="checkbox" name="color-black" id="color-black" />
-                <label htmlFor="color-black" />
-              </div>
-
-              <div className={styles.color}>
-                <input type="checkbox" name="color-white" id="color-white" />
-                <label htmlFor="color-white" />
-              </div>
-
-              <div className={styles.color}>
-                <input type="checkbox" name="color-gray" id="color-gray" />
-                <label htmlFor="color-gray" />
-              </div>
-
-              <div className={styles.color}>
-                <input type="checkbox" name="color-red" id="color-red" />
-                <label htmlFor="color-red" />
-              </div>
-
-              <div className={styles.color}>
-                <input type="checkbox" name="color-blue" id="color-blue" />
-                <label htmlFor="color-blue" />
-              </div>
-
-              <div className={styles.color}>
-                <input type="checkbox" name="color-green" id="color-green" />
-                <label htmlFor="color-green" />
-              </div>
-
-              <div className={styles.color}>
-                <input type="checkbox" name="color-yellow" id="color-yellow" />
-                <label htmlFor="color-yellow" />
-              </div>
-
-              <div className={styles.color}>
-                <input type="checkbox" name="color-purple" id="color-purple" />
-                <label htmlFor="color-purple" />
-              </div>
-
-              <div className={styles.color}>
-                <input type="checkbox" name="color-brown" id="color-brown" />
-                <label htmlFor="color-brown" />
-              </div>
-            </div>
-
-            <hr />
-          </div>
+          <Filter />          
 
           <div className={styles["products-list"]}>
             {loading ? (
