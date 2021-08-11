196c4bb0047ed8b1a7dcf579e4a6feba590f52dc
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2021-08-11 15:48:17 -0300

    style: create file to style the whatsapp icon

--- src/components/WhatsappIcon/index.tsx
+++ src/components/WhatsappIcon/index.tsx
@@ -1,8 +1,10 @@
 import { WhatsappIconProps } from "../../types/whatsapp";
 
+import styles from './styles.module.css';
+
 export default function WhatsappIcon(props: WhatsappIconProps) {
     return (
-        <div className="whatsapp-icon">
+        <div className={styles["whatsapp-icon"]}>
             <a href={`https://api.whatsapp.com/send?phone=${props.phone}&text=%20${props.message}`} target="_blank">
                 <img src="/icons/whatsapp-icon.svg" alt="Whatsapp-icon" />
             </a>
