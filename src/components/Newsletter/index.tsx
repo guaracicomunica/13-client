e2504234ed455cc3e2f79914a3b03da0bede9217
João Paulo <jpaulope@indracompany.com>
2021-08-11 11:58:46 -0300

    feat: notify error on newsletter subscription

--- src/components/Newsletter/index.tsx
+++ src/components/Newsletter/index.tsx
@@ -1,20 +1,49 @@
+import { useState } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+import { api } from "../../services/api";
 import styles from "./styles.module.css";
 
 export default function Newsletter() {
+
+    const [email, setEmail] = useState("");
+
+    async function handleSubmit(event: any) {
+        event.preventDefault();
+        try {
+            await api.post('newsletter/subscribe', {
+                email
+            });
+        } catch (error) {
+            toast.error('Erro ao se cadastrar na newsletter!', {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            });
+        }
+    }
+
     return (
         <div className={`py-5 ${styles.newsletter}`}>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="d-flex flex-column flex-lg-row align-items-center justify-content-around">
                     <h5 className="mb-3">Quero receber promos!</h5>
                     <div className="d-flex flex-column flex-md-row w-100">
                         <input
-                            type="text"
+                            type="email"
                             className="form-control mb-3 mr-4 ml-lg-4"
                             placeholder="Digite seu e-mail"
+                            value={email}
+                            onChange={e => setEmail(e.target.value)}
                         />
-                        <a href="#" className={`mb-3 button ${styles["newsletter-btn"]}`}>
+                        <button type="submit" className={`mb-3 button ${styles["newsletter-btn"]}`}>
                             Receber
-                        </a>
+                        </button>
                     </div>
                 </div>
 
@@ -34,6 +63,7 @@ export default function Newsletter() {
                     </div>
                 </div>
             </form>
+            <ToastContainer />
         </div>
     );
 }
