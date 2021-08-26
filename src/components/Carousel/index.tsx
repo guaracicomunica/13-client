41ea6c9ac87da05f6d393a523fa3a67ee8caf173
Fabiana Pereira <fabiana.duarte@hotmail.com.br>
2021-08-26 20:58:34 -0300

    fix: carousel height and object-fit

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -6,49 +6,42 @@ export default function Carousel() {
       className={`${styles['carousel-dimensions']} carousel slide`}
       data-ride="carousel"
     >
-      <div>
-        <div>
-          <ol className="carousel-indicators">
-            <li
-              data-target="#carousel-home"
-              data-slide-to="0"
-              className={`${styles["carousel-item-indicator"]} active`}
-            />
-            <li
-              data-target="#carousel-home"
-              data-slide-to="1"
-              className={styles["carousel-item-indicator"]}
-            />
-            <li
-              data-target="#carousel-home"
-              data-slide-to="2"
-              className={styles["carousel-item-indicator"]}
-            />
-          </ol>
+      <ol className="carousel-indicators">
+        <li
+          data-target="#carousel-home"
+          data-slide-to="0"
+          className={`${styles["carousel-item-indicator"]} active`}
+        />
+        <li
+          data-target="#carousel-home"
+          data-slide-to="1"
+          className={styles["carousel-item-indicator"]}
+        />
+        <li
+          data-target="#carousel-home"
+          data-slide-to="2"
+          className={styles["carousel-item-indicator"]}
+        />
+      </ol>
 
-          <div className="carousel-inner">
-            <div className={`carousel-item active ${styles['carousel-dimensions']}`}>
-              <img
-                className="d-block w-100"
-                src="/images/banner-home.svg"
-                alt="Primeiro Slide"
-              />
-            </div>
-            <div className={`carousel-item ${styles['carousel-dimensions']}`}>
-              <img
-                className="d-block w-100"
-                src="/images/banner-home.svg"
-                alt="Segundo Slide"
-              />
-            </div>
-            <div className={`carousel-item ${styles['carousel-dimensions']}`}>
-              <img
-                className="d-block w-100"
-                src="/images/banner-home.svg"
-                alt="Terceiro Slide"
-              />
-            </div>
-          </div>
+      <div className="carousel-inner">
+        <div className={`carousel-item active ${styles['carousel-dimensions']}`}>
+          <img
+            src="/images/banner-home.svg"
+            alt="Primeiro Slide"
+          />
+        </div>
+        <div className={`carousel-item ${styles['carousel-dimensions']}`}>
+          <img
+            src="/images/banner-home.svg"
+            alt="Segundo Slide"
+          />
+        </div>
+        <div className={`carousel-item ${styles['carousel-dimensions']}`}>
+          <img
+            src="/images/banner-home.svg"
+            alt="Terceiro Slide"
+          />
         </div>
       </div>
     </section>
